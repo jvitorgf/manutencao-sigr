3d836541e9bacf4ff31aa4a1179d8317594ecf98
jvitorgf <59662112+jvitorgf@users.noreply.github.com>
2021-05-10 19:50:32 -0300

    Pequenos ajustes

--- src/view/Administrador/index.js
+++ src/view/Administrador/index.js
@@ -92,10 +92,10 @@ function Administrador() {
                     })
                 }
 
-                if (consulta !== consulta[0].toUpperCase() + consulta[1] + consulta[2] + consulta[3] + consulta[4] + consulta[5] + consulta[6]) {
+                if (consulta !== consulta[0].toUpperCase() + consulta.slice(1).toLowerCase()) {
 
                     resultado.docs.forEach(doc => {
-                        if (doc.data().nome.indexOf(consulta[0].toUpperCase() + consulta[1] + consulta[2] + consulta[3] + consulta[4] + consulta[5] + consulta[6]) >= 0) {
+                        if (doc.data().nome.indexOf(consulta[0].toUpperCase() + consulta.slice(1).toLowerCase()) >= 0) {
                             listaItem.push({
                                 id: doc.id,
                                 ...doc.data()
